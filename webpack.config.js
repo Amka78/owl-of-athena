b32ec0aa81d6ecab8b175dd1480eb5fc9d736c02
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-07 11:30:44 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+
+module.exports = async function(env, argv) {
+    const config = await createExpoWebpackConfigAsync(env, argv);
+    console.debug(config);
+    // Customize the config before returning it.
+    return config;
+};
