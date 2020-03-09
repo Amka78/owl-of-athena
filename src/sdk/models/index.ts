383e49f99f564070efd16befe7ecc319af27d73d
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-10 00:56:00 +0900

    Add sdk/models/index.ts.

---
+++ src/sdk/models/index.ts
@@ -0,0 +1,5 @@
+import { AuroraOSInfo } from "./AuroraOSInfo";
+import { Settings } from "./Settings";
+import { UserProfile } from "./UserProfile";
+import { Profile } from "./Profile";
+export { AuroraOSInfo, UserProfile, Settings, Profile };
