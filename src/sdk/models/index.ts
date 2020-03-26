795e874ca927c8f6282fa8e3148b0e9498fcedfc
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-26 18:03:37 +0900

    Add AuroraSessionModel.

--- src/sdk/models/index.ts
+++ src/sdk/models/index.ts
@@ -1,6 +1,7 @@
 import { AuroraOSInfo } from "./AuroraOSInfo";
 import { Event } from "./Event";
+import { AuroraSession } from "./AuroraSession";
 import { Settings } from "./Settings";
 import { Profile } from "./Profile";
 
-export { AuroraOSInfo, Event, Settings, Profile };
+export { AuroraOSInfo, AuroraSession, Event, Settings, Profile };
