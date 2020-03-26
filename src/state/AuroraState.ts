1a4538f72ee37cbc374c624107b37a6960cfd132
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-26 18:08:30 +0900

    Add SessionList to State.

--- src/state/AuroraState.ts
+++ src/state/AuroraState.ts
@@ -1,8 +1,9 @@
-import { Settings } from "../sdk/models";
+import { AuroraSession, Settings } from "../sdk/models";
 import { AuroraProfile } from "../sdk/AuroraTypes";
 
 export type AuroraState = {
     userSettings: Settings;
     settingList: Array<Settings>;
     profileList: Array<AuroraProfile>;
+    sessionList: Array<AuroraSession>;
 };
