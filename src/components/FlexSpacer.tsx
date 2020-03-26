4d973129e4d665318d11ea7828808c892f62537b
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-26 18:10:42 +0900

    Add FlexSpacer component.

---
+++ src/components/FlexSpacer.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from "react";
+import { View } from "react-native";
+export const FlexSpacer: FunctionComponent = () => {
+    return <View style={{ flex: 1 }}></View>;
+};
