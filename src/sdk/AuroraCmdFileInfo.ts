811eff2e0ed15f12c8e9972c254932b42537d756
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-27 16:29:42 +0900

    Add AuroraCmdFileInfo.

---
+++ src/sdk/AuroraCmdFileInfo.ts
@@ -0,0 +1,23 @@
+import { ConnectorTypes } from "./AuroraConstants";
+import { Aurora } from "./Aurora";
+import { FileInfo, CommandResult } from "./AuroraTypes";
+
+const AuroraCmdFileInfo = async function(
+    this: Aurora,
+    srcPath: string,
+    connectorType: ConnectorTypes = ConnectorTypes.ANY
+): Promise<FileInfo> {
+    const srcPathSegments = srcPath.split("/");
+
+    const srcFileName = srcPathSegments.pop();
+    const srcFileDir = srcPathSegments.length ? srcPathSegments.join("/") : "/";
+
+    return (
+        await this.queueCmd<CommandResult<FileInfo>>(
+            `sd-file-info ${srcFileName} ${srcFileDir}`,
+            connectorType
+        )
+    ).response!;
+};
+
+export default AuroraCmdFileInfo;
