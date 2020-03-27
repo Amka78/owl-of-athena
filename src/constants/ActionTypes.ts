e2aa4b77193294a00135d4d86a7adad54da11c4c
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-27 16:40:53 +0900

    Added session related actions.

--- src/constants/ActionTypes.ts
+++ src/constants/ActionTypes.ts
@@ -4,11 +4,13 @@ const LOGOUT = "LOGOUT" as const;
 const UPDATE_SETTINGS = "UPDATE_SETTINGS" as const;
 const INIT = "INIT" as const;
 const UPDATE_PROFILES = "UPDATE_PROFILES" as const;
+const UPDATE_SESSIONS = "UPDATE_SESSION" as const;
 export default {
     INIT,
     LOGIN,
     LOGOUT,
     UPDATE_USER,
+    UPDATE_SESSIONS,
     UPDATE_SETTINGS,
     UPDATE_PROFILES
 };
