c45eaeb1f8d8b8e62cc3f76399085b82c5c9818a
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-03-27 16:38:49 +0900

    Fixed LoadingDialog to be displayed in a format similar to Alert.

--- src/components/LoadingDialog.tsx
+++ src/components/LoadingDialog.tsx
@@ -1,43 +1,69 @@
-import React, { FunctionComponent } from "react";
+import React from "react";
 import { Message, Colors, Fonts } from "../constants";
 import { MessageLocalizationParam } from "../constants/Message";
 import { Dialog, ActivityIndicator } from "react-native-paper";
-type LoadingDialogProps = {
-    visible: boolean;
+import { StyleSheet } from "react-native";
+
+type LoadingDialogState = {
+    settings?: LoadingDialogSettings;
+};
+
+type LoadingDialogSettings = {
     onDissmiss?: () => void;
     dialogTitle: MessageLocalizationParam;
 };
 
-export const LoadingDialog: FunctionComponent<LoadingDialogProps> = (
-    props: LoadingDialogProps
-) => {
-    return (
-        <Dialog
-            visible={props.visible}
-            onDismiss={async (): Promise<void> => {
-                if (props.onDissmiss) {
-                    props.onDissmiss();
-                }
-            }}
-            style={{
-                alignSelf: "center",
-                backgroundColor: Colors.navy_darker
-            }}
-        >
-            <Dialog.Title
-                style={{
-                    color: Colors.cyan,
-                    fontFamily: Fonts.primarySemiBold
-                }}
-            >
-                {Message.get(
-                    props.dialogTitle.key,
-                    props.dialogTitle.restParam
-                )}
-            </Dialog.Title>
-            <Dialog.Content>
-                <ActivityIndicator></ActivityIndicator>
-            </Dialog.Content>
-        </Dialog>
-    );
-};
+type LoadingDialogProps = {};
+
+export class LoadingDialog extends React.Component<
+    LoadingDialogProps,
+    LoadingDialogState
+> {
+    public static Instance?: LoadingDialog;
+
+    public static show(args: LoadingDialogSettings): void {
+        LoadingDialog.Instance!.setState({ settings: args });
+    }
+
+    public static close(): void {
+        LoadingDialog.Instance!.setState({ settings: undefined });
+    }
+
+    constructor(props: LoadingDialogProps) {
+        super(props);
+        this.state = {
+            settings: undefined
+        };
+    }
+
+    public async componentDidMount(): Promise<void> {
+        LoadingDialog.Instance = this;
+    }
+
+    public render(): JSX.Element | null {
+        return this.state.settings ? (
+            <Dialog visible={true} style={style.dialogContainer}>
+                <Dialog.Title style={style.dialogTitle}>
+                    {Message.get(
+                        this.state.settings!.dialogTitle.key,
+                        this.state.settings!.dialogTitle.restParam
+                    )}
+                </Dialog.Title>
+                <Dialog.Content>
+                    <ActivityIndicator></ActivityIndicator>
+                </Dialog.Content>
+            </Dialog>
+        ) : null;
+    }
+}
+
+const style = StyleSheet.create({
+    dialogContainer: {
+        alignSelf: "center",
+        backgroundColor: Colors.navy_darker
+    },
+    dialogTitle: {
+        color: Colors.cyan,
+        fontFamily: Fonts.primarySemiBold
+    }
+});
