a113124a01c9588bcad231211afa25c69515b3de
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-04-29 19:25:03 +0900

    Improved type definition.

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { AppRegistry } from "react-native";
 import App from "./App";
 
+declare let document: any;
 AppRegistry.registerComponent("Aurora-Web", () => App);
 AppRegistry.runApplication("Aurora-Web", {
-    // @ts-ignore
-    rootTag: document.querySelector("main")
+    rootTag: document.querySelector("main"),
 });
