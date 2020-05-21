b6728ee69888c01177b88417a5de1b2a25b67373
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-05-21 22:47:20 +0900

    Introduction of Preact.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-module.exports = async function (env, argv) {
+module.exports = async (env, argv) => {
     const config = await createExpoWebpackConfigAsync(env, argv);
-    console.debug(config);
-    // Customize the config before returning it.
+
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        // Use Preact aliases
+        react$: "preact/compat",
+        "react-dom$": "preact/compat",
+        // Fix the responder system which react-native-web depends on
+        "react-dom/unstable-native-dependencies$":
+            "preact-responder-event-plugin",
+    };
+
+    // Optionally you can enable the bundle size report.
+    // It's best to do this only with production builds.
+    // If you use it, be sure to install it with `yarn add -D webpack-bundle-analyzer`
+    if (env.mode === "production") {
+        config.plugins.push(
+            new BundleAnalyzerPlugin({
+                path: "web-report",
+            })
+        );
+    }
     return config;
 };
