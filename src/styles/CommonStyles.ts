08cc709d3f0b27f967e10c9d312ef94ac19be0ec
Amemiya Hikaru <amemiya@aracan.co.jp>
2020-05-21 22:02:11 +0900

    Delete unused styles.

--- src/styles/CommonStyles.ts
+++ src/styles/CommonStyles.ts
@@ -1,25 +1,11 @@
+//#region Import modules
 import { StyleSheet } from "react-native";
 
-import { Colors, Dimens, Layout } from "../constants";
+import { Colors, Dimens } from "../constants";
+//#endregion
 
+//#retgion StyleSheet
 export default StyleSheet.create({
-    bgImage: {
-        alignItems: "center",
-        flex: 1,
-        left: 0,
-        top: 0,
-        width: Layout.window.width,
-    },
-    centeredColumnList: {
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "space-evenly",
-    },
-    centeredContent: {
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center",
-    },
     standardView: {
         alignItems: "center",
         backgroundColor: Colors.navy,
@@ -27,19 +13,6 @@ export default StyleSheet.create({
         marginRight: Dimens.content_margin_horizontal,
         flex: 1,
         justifyContent: "space-between",
-    },
-    footerButton: {
-        alignItems: "center",
-        paddingBottom: 4,
-    },
-    form: {
-        borderColor: "white",
-        borderRadius: 10,
-        borderWidth: 1,
-        justifyContent: "center",
-        left: 10,
-        position: "absolute",
-        right: 10,
     },
     headerStyle: {
         backgroundColor: Colors.navy,
@@ -54,16 +27,9 @@ export default StyleSheet.create({
     headerLeftContainerStyle: {
         width: Dimens.content_margin_horizontal,
     },
-    input: {
-        borderBottomWidth: 1,
-        borderColor: "white",
-        borderWidth: 0,
-        height: 40,
-        marginLeft: 25,
-        width: "85%",
-    },
     rootContainer: {
         backgroundColor: Colors.navy,
         flex: 1,
     },
 });
+//#endregion
