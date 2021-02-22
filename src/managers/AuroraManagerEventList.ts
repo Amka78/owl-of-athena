941ed032c116212eef23998ebba03181be1afe2a
Amemiya Hikaru <amemiya@aracan.co.jp>
2021-02-23 03:08:35 +0900

    Carve out AuroraManagerEventList and make it common for each platform.

---
+++ src/managers/AuroraManagerEventList.ts
@@ -0,0 +1,10 @@
+export enum AuroraManagerEventList {
+    onConnectionChange = "onConnectionChange",
+    onSleepStateChange = "onSleepStateChnage",
+    onFoundUnsyncedSession = "onFoundUnsyncedSession",
+    onSleeping = "onSleeping",
+    onWaking = "onWaking",
+    onAwake = "onAwake",
+    onPushedSession = "onPushedSession",
+    onBatteryChange = "onBatteryChange",
+}
