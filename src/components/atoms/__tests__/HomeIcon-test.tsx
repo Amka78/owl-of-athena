ec195719081db60babb3c0bf8286cd9d07a8194d
Amemiya Hikaru <amemiya@aracan.co.jp>
2021-03-25 21:26:13 +0900

    Add HomeIcon unittest.

---
+++ src/components/atoms/__tests__/HomeIcon-test.tsx
@@ -0,0 +1,20 @@
+//#region Import Modules
+import "react-native";
+
+import { ShallowWrapper } from "enzyme";
+import React from "react";
+
+import { createMock, toJson } from "../../../utils/TestHelper";
+import { HomeIcon, HomeIconProps } from "../HomeIcon";
+//#endregion
+
+//#region Test
+let component: ShallowWrapper<HomeIconProps, unknown, unknown>;
+describe("HomeIcon UnitTest", () => {
+    it("renders correctly", () => {
+        component = createMock(<HomeIcon></HomeIcon>);
+
+        expect(toJson(component)).toMatchSnapshot();
+    });
+});
+//#endregion
