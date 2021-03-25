0ad6041360a6b75f4f497680dec80dcc52f39164
Amemiya Hikaru <amemiya@aracan.co.jp>
2021-03-25 21:23:35 +0900

    Add CommunityIcon unittest.

---
+++ src/components/atoms/__tests__/CommunityIcon-test.tsx
@@ -0,0 +1,20 @@
+//#region Import Modules
+import "react-native";
+
+import { ShallowWrapper } from "enzyme";
+import React from "react";
+
+import { createMock, toJson } from "../../../utils/TestHelper";
+import { CommunityIcon, CommunityIconProps } from "../CommunityIcon";
+//#endregion
+
+//#region Test
+let component: ShallowWrapper<CommunityIconProps, unknown, unknown>;
+describe("CommunityIcon UnitTest", () => {
+    it("renders correctly", () => {
+        component = createMock(<CommunityIcon></CommunityIcon>);
+
+        expect(toJson(component)).toMatchSnapshot();
+    });
+});
+//#endregion
