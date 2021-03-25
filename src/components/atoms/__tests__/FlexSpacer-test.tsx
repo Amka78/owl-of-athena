bf9a41e6295a944993781cdfe7483016ffe1ede4
Amemiya Hikaru <amemiya@aracan.co.jp>
2021-03-25 21:25:31 +0900

    Add FlexSpacer unittest.

---
+++ src/components/atoms/__tests__/FlexSpacer-test.tsx
@@ -0,0 +1,20 @@
+//#region Import Modules
+import "react-native";
+
+import { ShallowWrapper } from "enzyme";
+import React from "react";
+
+import { createMock, toJson } from "../../../utils/TestHelper";
+import { FlexSpacer } from "../FlexSpacer";
+//#endregion
+
+//#region Test
+let component: ShallowWrapper<unknown, unknown, unknown>;
+describe("FlexSpacer UnitTest", () => {
+    it("renders correctly", () => {
+        component = createMock(<FlexSpacer>{"test"}</FlexSpacer>);
+
+        expect(toJson(component)).toMatchSnapshot();
+    });
+});
+//#endregion
